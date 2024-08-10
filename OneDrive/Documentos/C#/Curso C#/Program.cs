8ff61c4b75befaba86833da55b0d554e1f44f42d
SergioGTF <sergio23300033@aluno.cesupa.br>
2024-08-10 17:14:05 -0300

    Adicione README.md.

---
+++ OneDrive/Documentos/C#/Curso C#/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Curso_Csharp_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Agora posso começar");
+            Console.ReadLine();
+            Console.WriteLine(20 + 5);
+            Console.ReadLine();
+        }
+    }
+}
